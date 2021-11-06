f62b968afee7c4846b3b6b85643d1237a20bced4
Neel Adwani <nprosofficial@gmail.com>
2021-11-06 06:51:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+app = Flask('app', template_folder='templates')
+
+@app.route('/')
+def hello_world():
+  return render_template('index.html')
+
+app.run(host='0.0.0.0', port=8080)
