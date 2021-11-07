099d4a0cef60c627e0dbf0e3e8d525d88343bcea
Neel Adwani <nprosofficial@gmail.com>
2021-11-07 06:13:26 +0000

    added twilio API

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from flask import Flask, render_template, request
+from twilio.rest import Client
+import os
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+def twilioSend(mes, number):
+  message = client.messages \
+                  .create(
+                      body=mes,
+                      from_='+12815476047',
+                      to=number
+                  )
+  print(message.sid)
+
 app = Flask('app', template_folder='templates', static_folder='static')
 
 @app.route('/')
@@ -11,6 +27,7 @@ def hello_world_post():
   doge = request.form['doge']
   if str(btc) == "1337" and str(doge) == "69420":
     # return redirect(url_for('upload'))
+    twilioSend('lmao noob', '+917976066450')
     return "upload portal here"
   else:
     return "access denied"
